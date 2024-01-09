4380b0d84d4e038caf7adfe2be816bc6878520de
Usagi no Niku <chenbohan911@163.com>
2024-01-09 23:22:31 +0800

    fix outdated combat renderer

--- src/main/java/trou/fantasy_metropolis/render/combat/WhiterCombatRenderer.java
+++ src/main/java/trou/fantasy_metropolis/render/combat/WhiterCombatRenderer.java
@@ -10,7 +10,11 @@ import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.NotNull;
+import trou.fantasy_metropolis.Registries;
+import trou.fantasy_metropolis.capability.IContainerCapability;
+import trou.fantasy_metropolis.capability.SimpleContainer;
 import trou.fantasy_metropolis.item.ItemSwordWhiter;
 
 public class WhiterCombatRenderer extends RenderLayer<AbstractClientPlayer, PlayerModel<AbstractClientPlayer>> {
@@ -21,14 +25,18 @@ public class WhiterCombatRenderer extends RenderLayer<AbstractClientPlayer, Play
     @Override
     public void render(@NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int packedLight, @NotNull AbstractClientPlayer livingEntity, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch) {
         if (Minecraft.getInstance().player != null) {
-            var item = Minecraft.getInstance().player.getInventory().getItem(41);
-            if (item.getItem() instanceof ItemSwordWhiter) {
-                poseStack.pushPose();
-                poseStack.translate(0, 0.35, 0.25);
-                poseStack.rotateAround(Axis.YP.rotationDegrees(180F), 0F, 1F, 0F);
-                Minecraft.getInstance().getItemRenderer().renderStatic(item, ItemDisplayContext.FIXED, packedLight, OverlayTexture.NO_OVERLAY, poseStack, buffer, livingEntity.level(), 0);
-                poseStack.popPose();
-            }
+            LazyOptional<IContainerCapability> lazyContainerCap = Minecraft.getInstance().player.getCapability(Registries.FM_CONTAINER);
+            lazyContainerCap.ifPresent((containerCap) -> {
+                SimpleContainer container = containerCap.getContainer();
+                var item = container.getItem(0);
+                if (item.getItem() instanceof ItemSwordWhiter) {
+                    poseStack.pushPose();
+                    poseStack.translate(0, 0.35, 0.25);
+                    poseStack.rotateAround(Axis.YP.rotationDegrees(180F), 0F, 1F, 0F);
+                    Minecraft.getInstance().getItemRenderer().renderStatic(item, ItemDisplayContext.FIXED, packedLight, OverlayTexture.NO_OVERLAY, poseStack, buffer, livingEntity.level(), 0);
+                    poseStack.popPose();
+                }
+            });
         }
     }
 }
